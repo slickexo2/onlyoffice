ee15931f0b656305c149fa163aeb4253b765a167
Peter Nedonosko <pnedonosko@exoplatform.com>
2017-10-11 12:19:23 +0300

    Handle properly not changed document status (users is null from DS)

--- services/src/main/java/org/exoplatform/onlyoffice/rest/EditorService.java
+++ services/src/main/java/org/exoplatform/onlyoffice/rest/EditorService.java
@@ -182,9 +182,10 @@ public class EditorService implements ResourceContainer {
         String statusKey = (String) jsonObj.get("key");
         long statusCode = (long) jsonObj.get("status");
         String statusUrl = (String) jsonObj.get("url");
+        // Oct 2017: When Document server calls with status 4 (user closed w/o modification), the users array will be null 
         JSONArray statusUsersArray = (JSONArray) jsonObj.get("users");
-        String[] statusUsers = new String[statusUsersArray.size()];
-        statusUsersArray.toArray(statusUsers);
+        @SuppressWarnings("unchecked")
+        String[] statusUsers = statusUsersArray != null ? (String[]) statusUsersArray.toArray(new String[statusUsersArray.size()]) : new String[0];
 
         if (key != null && key.length() > 0) {
           if (userId != null && userId.length() > 0) {
