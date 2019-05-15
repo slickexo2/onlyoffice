23d3c60294ce6eadae081d324f8ec28f3bea0856
VladMartyshevskyi <vladmartishevskii@gmail.com>
2019-05-15 15:23:31 +0300

    Refactored

--- services/src/test/java/org/exoplatform/onlyoffice/EditorServiceTest.java
+++ services/src/test/java/org/exoplatform/onlyoffice/EditorServiceTest.java
@@ -1,31 +1,20 @@
 package org.exoplatform.onlyoffice;
 
-import java.io.ByteArrayInputStream;
-import java.net.URI;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.junit.Test;
 
-import org.exoplatform.commons.testing.BaseCommonsTestCase;
-import org.exoplatform.component.test.ConfigurationUnit;
-import org.exoplatform.component.test.ConfiguredBy;
-import org.exoplatform.component.test.ContainerScope;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.onlyoffice.rest.EditorService;
-import org.exoplatform.onlyoffice.test.OnlyofficeMockHttpServletRequest;
-import org.exoplatform.services.rest.impl.ContainerRequest;
+import org.exoplatform.onlyoffice.test.AbstractResourceTest;
+import org.exoplatform.services.jcr.ext.app.SessionProviderService;
 import org.exoplatform.services.rest.impl.ContainerResponse;
-import org.exoplatform.services.rest.impl.EnvironmentContext;
-import org.exoplatform.services.rest.impl.InputHeadersMap;
 import org.exoplatform.services.rest.impl.MultivaluedMapImpl;
 import org.exoplatform.services.rest.impl.RequestHandlerImpl;
-import org.exoplatform.services.rest.tools.DummyContainerResponseWriter;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -33,10 +22,8 @@ import io.jsonwebtoken.security.Keys;
 /**
  * The Class EditorServiceTest.
  */
-@ConfiguredBy({ @ConfigurationUnit(scope = ContainerScope.ROOT, path = "conf/test-configuration.xml"),
-    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
-    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/test/test-configuration.xml") })
-public class EditorServiceTest extends BaseCommonsTestCase {
+
+public class EditorServiceTest extends AbstractResourceTest {
 
   /** The Constant RESOURCE_URL. */
   protected static final String RESOURCE_URL = "/onlyoffice/editor";
@@ -50,9 +37,7 @@ public class EditorServiceTest extends BaseCommonsTestCase {
   /** The editor service. */
   protected EditorService       editorService;
 
-  /** The request handler. */
-  protected RequestHandlerImpl  requestHandler;
-
+  
   /** The status json payload. */
   protected String              statusJsonPayload;
 
@@ -74,6 +59,7 @@ public class EditorServiceTest extends BaseCommonsTestCase {
   @Override
   protected void beforeClass() {
     super.beforeClass();
+    this.sessionProviderService = getContainer().getComponentInstanceOfType(SessionProviderService.class);
     this.container = PortalContainer.getInstance();
     ExoContainerContext.setCurrentContainer(container);
     requestHandler = getContainer().getComponentInstanceOfType(RequestHandlerImpl.class);
@@ -215,45 +201,5 @@ public class EditorServiceTest extends BaseCommonsTestCase {
     assertEquals("{\"error\":\"The token is not valid\"}", response.getEntity());
   }
 
-  /**
-   * Sends a request to specified URI.
-   *
-   * @param method the method
-   * @param requestURI the request URI
-   * @param baseURI the base URI
-   * @param headers the headers
-   * @param data the data
-   * @return the container response
-   * @throws Exception the exception
-   */
-  protected ContainerResponse service(String method,
-                                      String requestURI,
-                                      String baseURI,
-                                      Map<String, List<String>> headers,
-                                      byte[] data) throws Exception {
-
-    if (headers == null) {
-      headers = new MultivaluedMapImpl();
-    }
-
-    ByteArrayInputStream in = null;
-    if (data != null) {
-      in = new ByteArrayInputStream(data);
-    }
-
-    EnvironmentContext envctx = new EnvironmentContext();
-    HttpServletRequest httpRequest =
-                                   new OnlyofficeMockHttpServletRequest("", in, in != null ? in.available() : 0, method, headers);
-    envctx.put(HttpServletRequest.class, httpRequest);
-    EnvironmentContext.setCurrent(envctx);
-    ContainerRequest request = new ContainerRequest(method,
-                                                    new URI(requestURI),
-                                                    new URI(baseURI),
-                                                    in,
-                                                    new InputHeadersMap(headers));
-    ContainerResponse response = new ContainerResponse(new DummyContainerResponseWriter());
-    requestHandler.handleRequest(request, response);
-    return response;
-  }
   
 }
