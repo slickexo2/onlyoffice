bac3f2d52860da45255064bc79169afc2ce02d8c
VladMartyshevskyi <vladmartishevskii@gmail.com>
2019-07-03 17:36:49 +0300

    Added JUnit tests

--- services/src/test/java/org/exoplatform/onlyoffice/OnlyofficeEditorServiceTest.java
+++ services/src/test/java/org/exoplatform/onlyoffice/OnlyofficeEditorServiceTest.java
@@ -1,10 +1,15 @@
 package org.exoplatform.onlyoffice;
 
+import static org.mockito.Matchers.contains;
+
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 import javax.jcr.Node;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import org.exoplatform.commons.testing.BaseCommonsTestCase;
@@ -20,16 +25,23 @@ import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.services.security.Identity;
 import org.exoplatform.services.security.MembershipEntry;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
 @ConfiguredBy({ @ConfigurationUnit(scope = ContainerScope.ROOT, path = "conf/test-configuration.xml"),
     @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
     @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/test/test-configuration.xml") })
 public class OnlyofficeEditorServiceTest extends BaseCommonsTestCase {
 
   /** The Constant LOG. */
-  protected static final Log    LOG                    = ExoLogger.getLogger(OnlyofficeEditorServiceTest.class);
-  
+  protected static final Log        LOG        = ExoLogger.getLogger(OnlyofficeEditorServiceTest.class);
+
+  /** The Constant SECRET_KEY. */
+  protected static final String     SECRET_KEY = "1fRW5pBZu3UIBEdebbpDpKJ4hwExSQoSe97tw8gyYNhqnM1biHb";
+
   protected OnlyofficeEditorService editorService;
-  protected SessionProviderService sessionProviderService;
+
+  protected SessionProviderService  sessionProviderService;
 
   /**
    * Before class.
@@ -52,13 +64,13 @@ public class OnlyofficeEditorServiceTest extends BaseCommonsTestCase {
     contentNode.setProperty("jcr:lastModified", Calendar.getInstance());
     contentNode.setProperty("jcr:data", "testContent");
     session.save();
-    
+
     String docId = node.getUUID();
     Config config = editorService.createEditor("http", "127.0.0.1", 8080, "john", null, docId);
     String editorURL = "http://127.0.0.1:8080/portal/intranet/oeditor?docId=" + docId;
-    
+
     assertNotNull(config);
-    
+
     assertTrue(config.isCreated());
     assertFalse(config.isClosing());
     assertFalse(config.isOpen());
@@ -67,33 +79,124 @@ public class OnlyofficeEditorServiceTest extends BaseCommonsTestCase {
     assertEquals(docId, config.getDocId());
     assertEquals(editorURL, config.getEditorUrl());
     assertEquals("/Test Document.docx", config.getPath());
-    
+
     assertNotNull(config.getDocument());
     assertEquals("Test Document.docx", config.getDocument().getTitle());
     assertEquals("docx", config.getDocument().getFileType());
     assertEquals("john", config.getDocument().getInfo().getAuthor());
-    
+
     assertNotNull(config.getEditorConfig());
     assertNotNull(config.getEditorConfig().getUser());
     assertEquals("John", config.getEditorConfig().getUser().getFirstname());
     assertEquals("Smith", config.getEditorConfig().getUser().getLastname());
+    node.remove();
+    session.save();
   }
-  
+
   @Test
   public void testCreateEditorIncorrectFile() throws Exception {
     startSessionAs("john");
-    Node node = session.getRootNode().addNode("Test Document.docx", "nt:base");
-    node.addMixin("mix:referenceable");
-    session.save();
+    Node node = createDocument("Test Document.docx", "nt:base", "testContent");
     try {
-      editorService.createEditor("http", "127.0.0.1", 8080, "john", null, node.getUUID());  
+      editorService.createEditor("http", "127.0.0.1", 8080, "john", null, node.getUUID());
     } catch (OnlyofficeEditorException e) {
       // Ok
+      node.remove();
+      session.save();
       return;
     }
     // Fail if the exception wasn't thrown
     fail();
   }
+
+  @Test
+  public void testGetContent() throws Exception {
+    startSessionAs("john");
+    Node node = createDocument("Test Document.docx", "nt:file", "testContent");
+    Config config = editorService.createEditor("http", "127.0.0.1", 8080, "john", null, node.getUUID());
+
+    DocumentContent documentContent = editorService.getContent("john", config.getDocument().getKey());
+
+    assertNotNull(documentContent);
+    String content = IOUtils.toString(documentContent.getData(), "UTF-8");
+    assertEquals("testContent", content);
+    assertEquals("application/vnd.oasis.opendocument.text", documentContent.getType());
+    node.remove();
+  }
+
+  @Test
+  public void testIsDocumentMimeSupported() throws Exception {
+    startSessionAs("john");
+    Node node = createDocument("Test Document.docx", "nt:file", "testContent");
+    assertTrue(editorService.isDocumentMimeSupported(node));
+    node.getNode("jcr:content").setProperty("jcr:mimeType", "text/plain");
+    session.save();
+    assertFalse(editorService.isDocumentMimeSupported(node));
+    Node unsctructuredNode = session.getRootNode().addNode("Test Document.docx", "nt:unstructured");
+    session.save();
+    assertTrue(editorService.isDocumentMimeSupported(unsctructuredNode));
+    node.remove();
+    unsctructuredNode.remove();
+  }
+
+  @Test
+  public void testCanEditDocument() throws Exception {
+    startSessionAs("john");
+    Node node = createDocument("Test Document.docx", "nt:file", "testContent");
+
+    assertTrue(editorService.canEditDocument(node));
+    node.lock(true, false);
+    assertFalse(editorService.canEditDocument(node));
+    node.remove();
+  }
+
+  @Test
+  public void testValidateToken() throws Exception {
+    startSessionAs("john");
+    Node node = createDocument("Test Document.docx", "nt:file", "testContent");
+    Config config = editorService.createEditor("http", "127.0.0.1", 8080, "john", null, node.getUUID());
+    String key = config.getDocument().getKey();
+    Map<String, Object> payload = new HashMap<>();
+    
+    payload.put("key", key);
+
+    String token = Jwts.builder()
+                       .setSubject("exo-onlyoffice")
+                       .claim("payload", payload)
+                       .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                       .compact();
+    
+    String wrongSignToken= Jwts.builder()
+        .setSubject("exo-onlyoffice")
+        .claim("payload", payload)
+        .signWith(Keys.hmacShaKeyFor("WRONG-SECRET-KEY-hwExSQoSe97tw8gyYNhqnM1biHb".getBytes()))
+        .compact();
+    
+    payload.replace("key", "wrong-document-key");
+    String wrongKeyToken = Jwts.builder()
+        .setSubject("exo-onlyoffice")
+        .claim("payload", payload)
+        .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+        .compact();
+    
+    assertTrue(editorService.validateToken(token, key));
+    
+    assertFalse(editorService.validateToken(wrongSignToken, key));
+    assertFalse(editorService.validateToken(wrongKeyToken, key));
+    
+    payload.clear();
+    // URL should end with key
+    payload.put("url", "http://127.0.0.1:8080/editor/" + key);
+    
+    token = Jwts.builder()
+        .setSubject("exo-onlyoffice")
+        .claim("payload", payload)
+        .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+        .compact();
+    assertTrue(editorService.validateToken(token, key));
+  }
+  
+  
 
   protected void startSessionAs(String user) {
     HashSet<MembershipEntry> memberships = new HashSet<MembershipEntry>();
@@ -102,6 +205,21 @@ public class OnlyofficeEditorServiceTest extends BaseCommonsTestCase {
     ConversationState state = new ConversationState(identity);
     ConversationState.setCurrent(state);
     sessionProviderService.setSessionProvider(null, new SessionProvider(state));
+  }
+
+  protected Node createDocument(String title, String type, String data) throws Exception {
+    Node node = session.getRootNode().addNode(title, type);
+    node.addMixin("mix:lockable");
+    node.addMixin("mix:referenceable");
+    if (type.equals("nt:file")) {
+      Node contentNode = node.addNode("jcr:content", "nt:unstructured");
+      contentNode.setProperty("jcr:mimeType", "application/vnd.oasis.opendocument.text");
+      contentNode.setProperty("jcr:lastModified", Calendar.getInstance());
+      contentNode.setProperty("jcr:data", data);
+    }
+
+    session.save();
+    return node;
   }
 
 }
